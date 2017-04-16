94b4ec2f3ea7aebc80332de41920ebf647ea7496
Roald Umandal <umandalroald@gmail.com>
2017-04-16 08:50:29 +0800

    first commit

---
+++ templates/coming-soon-page.tpl.php
@@ -0,0 +1,19 @@
+<div class="topleft">
+  <p><% Roald R. Umandal %></p>
+</div>
+
+<div class="middle">
+  <h1>[COMING SOON]</h1>
+    <div class="divider"></div>
+  <p id="demo" style="font-size:30px"></p>
+</div>
+
+<div class="bottomleft">
+  <ul class="social-media-links">
+    <!--<li><a href="https://www.facebook.com/csrftokens">facebook</a></li>-->
+    <!--<li><a href="https://www.instagram.com/umandalroald/">instagram</a></li>-->
+    <li><a href="https://twitter.com/iamroald">twitter</a></li>
+    <li><a href="https://github.com/umandalroald">github</a></li>
+    <li><a href="https://www.linkedin.com/in/roald-umandal">linkedin</a></li>
+  </u>
+</div>
